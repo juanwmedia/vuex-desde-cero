4c1e438f85593ae6dd7252d704b05eb05eb560ce
Juan <juan@wmedia.,es>
2020-01-18 19:44:55 +0100

    Starting code

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AppProductList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import AppProductList from "./components/AppProductList.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    AppProductList
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/AppProductList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>Listado de productos</h1>
+    <hr />
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.title }} | {{ product.price }}
+        <i>{{ product.inventory }} </i>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import api from "../api/shop.js";
+export default {
+  name: "AppProductList",
+  data() {
+    return {
+      products: []
+    };
+  },
+  created() {
+    api.getProducts(products => {
+      this.products = products;
+    });
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  text-align: left;
+}
+</style>
